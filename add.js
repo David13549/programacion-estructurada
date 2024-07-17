106bcca9e8b699cad3bf1ea3e543eb68a969c5fa
David13549 <kv959674@gmail.com>
2024-07-17 10:21:01 -0600

    agregando cambios

---
+++ add.js
@@ -0,0 +1,10 @@
+ var unidad = "1"
+ var nombre = "kevin"
+
+var nombre ="nelson"
+
+ //git add . este comando nos permite inicializar un agregado
+
+ //  git commit -m "u comentario " nos peermite comentar lo que hicimos y vamos a subir
+
+ // git push -u origin main este aplica los cambios en github
